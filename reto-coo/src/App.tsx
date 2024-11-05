61b97e9e476a4645f868c7303d150ae1d20bacc0
Jose <josemase@unisabana.edu.co>
2024-11-04 22:46:24 -0500

    commit on dev

--- reto-coo/src/App.tsx
+++ reto-coo/src/App.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
 import { Image, Navbar } from 'react-bootstrap';
+import AppsIcon from '@mui/icons-material/Apps';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
 import './App.css';
 
 function App() {
   const [expandFalse, setExpandFalse] = useState(false);
   return (
     <>
-        <Navbar  expand={expandFalse} className='custom-navbar'>         
-            <Navbar.Toggle className="me-2" aria-controls={`offcanvasNavbar-expand-${expandFalse}`} />
-            <Image src="./image1.jpg" className="custom-image" rounded />          
-        </Navbar>
-      
+      <Navbar expand={expandFalse} className='custom-navbar'>
+        <Navbar.Toggle className="me-2" aria-controls={`offcanvasNavbar-expand-${expandFalse}`} />
+        <Image src="./image1.jpg" className="custom-image" rounded />
+        <div id='userName'>
+          <h6 className='userShown'>User 6</h6>
+          <p><b>Terminal:</b> 01 BOG</p>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+          <AccountCircleTwoToneIcon className='icons' />
+          <SearchOutlinedIcon className='icons' />
+          <AppsIcon className='icons' />
+        </div>
+      </Navbar>
     </>
   );
 }
